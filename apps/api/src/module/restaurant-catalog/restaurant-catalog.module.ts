aa3dc73bc8387be32f0fce85975c73ee66787eaa
Phạm Hùng <23520573@gm.uit.edu.vn>
2026-04-27 09:50:41 +0700

    implement search module, can search for restaurant

--- apps/api/src/module/restaurant-catalog/restaurant-catalog.module.ts
+++ apps/api/src/module/restaurant-catalog/restaurant-catalog.module.ts
@@ -1,8 +1,9 @@
 import { Module } from '@nestjs/common';
 import { MenuModule } from './menu/menu.module';
 import { RestaurantModule } from './restaurant/restaurant.module';
+import { SearchModule } from './search/search.module';
 
 @Module({
-  imports: [MenuModule, RestaurantModule],
+  imports: [MenuModule, RestaurantModule, SearchModule],
 })
 export class RestaurantCatalogModule {}
